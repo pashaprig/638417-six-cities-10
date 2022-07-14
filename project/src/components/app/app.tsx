f38e15c2c13422209f93376c94890a92e363477d
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-07-14 14:08:12 +0300

    3.5 Private-route for Favorite component route

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import { AppRoute } from '../../consts';
+import { AppRoute, AuthorizationStatus } from '../../consts';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import RoomScreen from '../../pages/room-screen/room-screen';
 import { FavoritesProps } from '../../types';
+import PrivateRoute from '../private-route/private-route';
 
 function App({ favoritesCount }: FavoritesProps): JSX.Element {
   return (
@@ -21,7 +22,13 @@ function App({ favoritesCount }: FavoritesProps): JSX.Element {
         />
         <Route
           path={AppRoute.Favorites}
-          element={<FavoritesScreen />}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.Auth}
+            >
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Room}
